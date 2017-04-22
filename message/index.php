29a12f451dff6f8fbd97e7869a256e84daca540f
Zhandra Habig <Zhandra Habig>
2017-04-22 11:27:39 +0800

    message

---
+++ message/index.php
@@ -0,0 +1,21 @@
+<?php
+include("include/configuration.php");
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="container-fluid">
+			<div class="col-md-4">
+				
+			</div>
+			<div class="col-md-8">
+				
+			</div>
+		</div>
+	</body>
+</html>
+
